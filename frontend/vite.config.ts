5908c753f32f9c5b2a65585b8314e35738dab07f
danjkim21 <dan.jkim21@gmail.com>
2022-10-16 14:37:21 -0400

    removed unnecessary dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,10 +17,5 @@ export default defineConfig({
 },
   plugins: [
     react(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-      },
-    }),
   ],
 })
