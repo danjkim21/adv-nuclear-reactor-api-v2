d6c9c57f601ebe33575ddcf288d99fdbaf326d3c
danjkim21 <dan.jkim21@gmail.com>
2022-10-19 11:57:56 -0400

    updated vite port config -test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: true,
-    port : 8000,
+    port : 3000,
     proxy: {
         '/api': {
             target: 'http://localhost:8000',
