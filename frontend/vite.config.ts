e32f9ada9e268593b325c2754d07b326ea0998a4
danjkim21 <dan.jkim21@gmail.com>
2022-10-19 13:04:24 -0400

    attemt to fix port and scripts -test 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,12 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: true,
-    port : 3000,
+    // port : 3000,
     proxy: {
         '/api': {
-            target: 'http://localhost:8000',
-            changeOrigin: true,
-            secure: false
+            target: 'http://localhost:8000/',
+            // changeOrigin: true,
+            // secure: false
         }
     }
     
